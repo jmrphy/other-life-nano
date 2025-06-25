bbeebb3d91ebeca3f34d86945d51d30f34690813
Justin Murphy <jmurphydata@gmail.com>
2025-06-24 21:07:23 -0500

    adding plausible

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://justinmurphy.studio", // Your custom domain
   integrations: [mdx(), sitemap(), tailwind()],
 });
